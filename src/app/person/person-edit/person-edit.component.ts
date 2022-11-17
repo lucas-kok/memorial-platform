e1b7650bacfc7b546a30db5325dc842771fbfa15
lucas-kok <lucas.kok@hotmail.nl>
2022-11-17 21:33:34 +0100

    displayed personal information

--- src/app/person/person-edit/person-edit.component.ts
+++ src/app/person/person-edit/person-edit.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Person } from '../person.model';
+import { PersonService } from '../person.service';
 
 @Component({
   selector: 'app-person-edit',
   templateUrl: './person-edit.component.html',
-  styleUrls: ['./person-edit.component.css']
+  styleUrls: ['./person-edit.component.css'],
 })
 export class PersonEditComponent {
+  componentId: string | null | undefined;
+  person: Person | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private personService: PersonService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.componentId = params.get('id');
+
+      if (this.componentId == null) return;
+
+      this.person = this.personService.getPersonById(this.componentId);
+    });
+  }
+
+  onDelete() {
+    // Person must exist for removing
+    if (this.person != null && this.componentId != null) {
+      console.log('Removing user with id: ' + this.componentId);
+
+      this.personService.removePersonById(this.componentId);
+    }
+  }
 }
