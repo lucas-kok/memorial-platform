2705e96e799cb0f7c99c99fd74eb1f291678fb88
lucas-kok <lucas.kok@hotmail.nl>
2022-11-17 17:40:58 +0100

    added persons component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    PersonsComponent
   ],
   imports: [
     BrowserModule,
