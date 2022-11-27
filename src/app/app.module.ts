5255a5022311bc54eba69a5a11179a5636e52818
lucas-kok <56938053+lucas-kok@users.noreply.github.com>
2022-11-27 20:54:19 +0100

    added user edit component\

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './about/about.component';
 import { UserComponent } from './user/user.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { UserDetailsComponent } from './user/user-details/user-details.component
     UserComponent,
     UserCreateComponent,
     UserDetailsComponent,
+    UserEditComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
