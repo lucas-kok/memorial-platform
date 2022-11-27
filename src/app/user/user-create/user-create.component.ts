f31d2f0b48bfce8ec4fb5d22d220ba1114612db8
lucas-kok <56938053+lucas-kok@users.noreply.github.com>
2022-11-27 12:32:09 +0100

    added methods in user-create.component.ts

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Gender } from 'src/app/shared/gender.model';
+import { User } from '../user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.css']
+  styleUrls: ['./user-create.component.css'],
 })
 export class UserCreateComponent {
+  newUser: User | undefined;
+  genders: String[] | undefined;
 
+  constructor(private userService: UserService) {
+    this.newUser = new User();
+    this.genders = Object.values(Gender);
+  }
+
+  onCreate() {
+    if (this.newUser == null) return;
+
+    this.userService.addUser(this.newUser);
+  }
 }
