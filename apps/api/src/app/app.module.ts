8d40e11bf1f1513774f9bffd60f3b63acd7fb08c
lucas-kok <lucas.kok@hotmail.nl>
2022-12-01 10:28:48 +0100

    added Mongoose en bcrypt

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/authentication')],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
