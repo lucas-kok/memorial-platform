c682be2fec276954f049d3bd3a2887061a202999
lucas-kok <lucas.kok@hotmail.nl>
2022-12-01 10:37:07 +0100

    Added mongoose schema in users.module

--- apps/api/src/app/user/users.module.ts
+++ apps/api/src/app/user/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.model';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
 export class UsersModule {}
