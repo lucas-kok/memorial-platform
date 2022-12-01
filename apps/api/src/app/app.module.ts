388089e4c4c45f0aa9569e4d027b920fc877c30a
lucas-kok <lucas.kok@hotmail.nl>
2022-12-01 10:32:41 +0100

    namechanges of users folder

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsersController } from './user/users.controller';
+import { UsersService } from './user/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
