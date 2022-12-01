63e0f0b1cc9d5f47b443b4f3f4553a838ed37ead
lucas-kok <lucas.kok@hotmail.nl>
2022-12-01 11:36:21 +0100

    added validateUser method

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { UsersService } from '../user/users.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { User } from '../user/user.model';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,16 @@ export class AuthService {
     private readonly userService: UsersService,
     private jwtService: JwtService
   ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user: User = await this.userService.getUserByEmail(email);
+    if (!user) {
+      throw new NotAcceptableException('could not find the user');
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.passwordHash!);
+    if (passwordValid) {
+      return user;
+    }
+  }
 }
