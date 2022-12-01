5ecad2c4f601c243737cd0723a2682a88cc30215
lucas-kok <lucas.kok@hotmail.nl>
2022-12-01 11:26:44 +0100

    added validate function

--- apps/api/src/app/auth/local.auth.ts
+++ apps/api/src/app/auth/local.auth.ts
@@ -8,4 +8,13 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super();
   }
+
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
 }
