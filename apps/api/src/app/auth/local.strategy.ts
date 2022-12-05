ba50bf464629fdcdcc53bc9b7a1d7144a7eeef4e
lucas-kok <lucas.kok@hotmail.nl>
2022-12-05 15:57:38 +0100

    added validate method in local strategy

--- apps/api/src/app/auth/local.strategy.ts
+++ apps/api/src/app/auth/local.strategy.ts
@@ -1,8 +1,22 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { UserLoginDto } from '../user/user.dto';
+import { AuthService } from './auth.service';
 
+@Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private authService: AuthService) {
     super();
+  }
+
+  async validate(userLoginDto: UserLoginDto): Promise<any> {
+    const user = this.authService.validateUser(userLoginDto);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
   }
 }
