455894c581db73515200a8ab7fd0f9991e4f0a3d
lucas-kok <lucas.kok@hotmail.nl>
2022-12-05 16:04:29 +0100

    removed imports from authController

--- apps/api/src/app/auth/auth.controller.ts
+++ apps/api/src/app/auth/auth.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Post, UseGuards, Res, Body } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
 import { UserLoginDto } from '../user/user.dto';
-import { User } from '../user/user.model';
 
 @Controller('auth')
 export class AuthController {
