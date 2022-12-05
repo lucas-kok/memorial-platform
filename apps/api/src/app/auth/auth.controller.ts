511607fcdf6f3d332d0ff446f6c1e2d7e7908035
lucas-kok <lucas.kok@hotmail.nl>
2022-12-05 16:41:18 +0100

    removed local strategy

--- apps/api/src/app/auth/auth.controller.ts
+++ apps/api/src/app/auth/auth.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Post, UseGuards, Res, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Res,
+  Body,
+  Get,
+  Req,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { UserLoginDto } from '../user/user.dto';
-import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(@Body() userLoginDto: UserLoginDto, @Res() res: Response) {
     const user = await this.authService.validateUser(userLoginDto);
