7b34722e46b2d37ad73b03653044ed767dca990f
lucas-kok <lucas.kok@hotmail.nl>
2022-12-06 14:38:58 +0100

    added personDto

---
+++ apps/api/src/persons/person.dto.ts
@@ -0,0 +1,24 @@
+import { IsDateString, IsIn, IsNotEmpty, IsString } from 'class-validator';
+import { Gender } from '../app/shared/gender.model';
+
+export class PersonDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string | undefined;
+
+  @IsNotEmpty()
+  @IsDateString()
+  readonly birthday: Date | undefined;
+
+  @IsNotEmpty()
+  @IsDateString()
+  readonly deathday: Date | undefined;
+
+  @IsNotEmpty()
+  @IsIn(['male', 'female'])
+  readonly gender: Gender | undefined;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly imageBase64: string | undefined;
+}
