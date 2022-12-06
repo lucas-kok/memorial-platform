446e82847180590c56cc27c56baae7ed23f4db88
lucas-kok <lucas.kok@hotmail.nl>
2022-12-06 14:43:06 +0100

    created schema for person

--- apps/api/src/persons/person.model.ts
+++ apps/api/src/persons/person.model.ts
@@ -1,10 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Gender } from '../app/shared/gender.model';
 
+export type PersonDocument = Person & Document;
+
+@Schema()
 export class Person {
   _id: string | undefined;
+
+  @Prop({ type: 'string' })
   name: string | undefined;
+
+  @Prop({ type: 'Date' })
   birthday: Date | undefined;
+
+  @Prop({ type: 'Date' })
   deathday: Date | undefined;
+
+  @Prop({ type: 'string' })
   gender: Gender | undefined;
+
+  @Prop({ type: 'string' })
   imageBase64: string | undefined;
 }
+
+export const PersonSchema = SchemaFactory.createForClass(Person);
--- apps/api/src/persons/persons.service.ts
+++ apps/api/src/persons/persons.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PersonsService {
