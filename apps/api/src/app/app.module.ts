42ecf6bf9d856d61753542063aea3ec8abeb8e05
lucas-kok <lucas.kok@hotmail.nl>
2022-12-06 15:22:29 +0100

    added personModule to appModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth/auth.controller';
 import { UsersModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PersonModule } from './persons/person.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/authentication'),
     UsersModule,
     AuthModule,
+    PersonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
