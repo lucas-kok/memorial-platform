eab81d7d30dc99a01109a0b2b8715641197a7ac0
lucas-kok <lucas.kok@hotmail.nl>
2022-12-06 15:36:05 +0100

    added notations to routes of personsController

--- apps/api/src/app/persons/persons.controller.ts
+++ apps/api/src/app/persons/persons.controller.ts
@@ -1,25 +1,54 @@
-import { Controller, Delete, Get, Post, Put, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Post,
+  Put,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PersonDto } from './person.dto';
+import { PersonsService } from './persons.service';
 
 @Controller('persons')
 export class PersonsController {
+  constructor(private personsService: PersonsService) {}
+
   @UseGuards(JwtAuthGuard)
   @Post()
-  addPerson() {}
+  addPerson(@Body() personDto: PersonDto, @Res() res: Response) {
+    Logger.log('[PersonsController][POST]/persons called');
+    Logger.log(personDto);
+  }
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  getAllPersonsFromUser() {}
+  getAllPersonsFromUser(@Res() res: Response) {}
 
   @UseGuards(JwtAuthGuard)
   @Get(':id')
-  getPersonById() {}
+  getPersonById(
+    @Param(':id') id: string,
+    @Body() personDto: PersonDto,
+    @Res() res: Response
+  ) {
+    Logger.log('[PersonsController][GET]/persons/' + id + ' called');
+  }
 
   @UseGuards(JwtAuthGuard)
   @Put(':id')
-  updatePerson() {}
+  updatePerson(@Param('id') id: string, @Res() res: Response) {
+    Logger.log('[PersonsController][PUT]/persons/' + id + ' called');
+  }
 
   @UseGuards(JwtAuthGuard)
-  @Delete()
-  removePerson() {}
+  @Delete(':id')
+  removePerson(@Param('id') id: string, @Res() res: Response) {
+    Logger.log('[PersonsController][DELETE]/persons/' + id + ' called');
+  }
 }
