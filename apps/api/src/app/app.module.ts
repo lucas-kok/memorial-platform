1a7266cd9fb3fc7ab86784b0e95f71831fba4a8d
lucas-kok <lucas.kok@hotmail.nl>
2022-12-06 15:22:45 +0100

    updated imports from appModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './user/users.controller';
-import { UsersService } from './user/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthController } from './auth/auth.controller';
 import { UsersModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PersonModule } from './persons/person.module';
