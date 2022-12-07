1c3149181429e6b236cc2703961fbe8acdac15f7
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 16:47:04 +0100

    created schema for messageModel'

--- apps/api/src/app/messages/message.model.ts
+++ apps/api/src/app/messages/message.model.ts
@@ -1,9 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
 export class Message {
   _id: string | undefined;
+
+  @Prop({ type: 'string' })
   userId: string | undefined;
+
+  @Prop({ type: 'Date' })
   dateTime: Date | undefined;
+
+  @Prop({ type: 'string' })
   qoute: string | undefined;
+
+  @Prop({ type: 'string' })
   message: string | undefined;
+
+  @Prop({ type: 'string' })
   imageBase64: string | undefined;
+
+  @Prop({ type: 'boolean' })
   showName: boolean | undefined;
 }
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
