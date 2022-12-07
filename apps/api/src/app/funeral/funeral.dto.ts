cc4472738eeaf0c124bc9e7f15e26778d92a5952
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 11:20:41 +0100

    added class validation to funeralDto

--- apps/api/src/app/funeral/funeral.dto.ts
+++ apps/api/src/app/funeral/funeral.dto.ts
@@ -1,8 +1,27 @@
+import { IsBoolean, IsDateString, IsNotEmpty, IsString } from 'class-validator';
+
 export class FuneralDto {
+  @IsNotEmpty()
+  @IsDateString()
   readonly dateTime: Date | undefined;
+
+  @IsNotEmpty()
+  @IsString()
   readonly adress: string | undefined;
+
+  @IsNotEmpty()
+  @IsString()
   readonly postalCode: string | undefined;
+
+  @IsNotEmpty()
+  @IsString()
   readonly city: string | undefined;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string | undefined;
+
+  @IsNotEmpty()
+  @IsBoolean()
   readonly isPrivate: boolean | undefined;
 }
