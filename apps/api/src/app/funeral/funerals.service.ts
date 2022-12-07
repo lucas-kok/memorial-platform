ea1794cf6e9b141e28b511520274d357d34403f2
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 11:32:48 +0100

    added funeralModel in funeralsService

--- apps/api/src/app/funeral/funerals.service.ts
+++ apps/api/src/app/funeral/funerals.service.ts
@@ -1,7 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { FuneralDocument } from './funeral.model';
 
 @Injectable()
 export class FuneralsService {
+  constructor(
+    @InjectModel('funeral')
+    private readonly funeralModel: Model<FuneralDocument>
+  ) {}
+
   addFuneral() {
     Logger.log('[FuneralsService] addfuneral called');
   }
