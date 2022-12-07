a5e1b03da039e58369a708db72e9474737c2bf51
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 20:53:34 +0100

    added auhtController test

---
+++ apps/api/src/app/auth/auth.controller.spec.ts
@@ -0,0 +1,24 @@
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('AuhtController', () => {
+  let app: TestingModule;
+  let authController: AuthController;
+  let authService: AuthService;
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            validateUser: jest.fn(),
+            login: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+  });
+});
