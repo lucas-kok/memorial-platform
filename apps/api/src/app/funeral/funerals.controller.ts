d820762858b1b03beba9fdd355d1ab5ada7224b5
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 11:25:54 +0100

    added routes to funeralsController

--- apps/api/src/app/funeral/funerals.controller.ts
+++ apps/api/src/app/funeral/funerals.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { FuneralsService } from './funerals.service';
 
 @Controller('funerals')
 export class FuneralsController {
   constructor(private funeralsService: FuneralsService) {}
+
+  @Post()
+  addFuneral() {}
+
+  @Get()
+  getAllFunerals() {}
+
+  @Get(':id')
+  getFuneralById() {}
+
+  @Put()
+  updateFuneral() {}
+
+  @Delete(':id')
+  removeFuneralById() {}
 }
