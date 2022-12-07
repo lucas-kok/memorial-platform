bd86b1978b54cab76009c38332b980bb400ed010
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 14:56:22 +0100

    added schema to memorialModel

--- apps/api/src/app/memorials/memorial.model.ts
+++ apps/api/src/app/memorials/memorial.model.ts
@@ -1,8 +1,21 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+
+@Schema()
 export class Memorial {
   _id: string | undefined;
+
+  @Prop({ type: 'string ' })
   userId: string | undefined;
+
+  @Prop({ type: 'string ' })
   personId: string | undefined;
+
+  @Prop({ type: 'string ' })
   funeralId: string | undefined;
+
+  @Prop({ type: 'string ' })
   description: string | undefined;
+
+  @Prop({ type: 'string ' })
   imageBase64: string | undefined;
 }
