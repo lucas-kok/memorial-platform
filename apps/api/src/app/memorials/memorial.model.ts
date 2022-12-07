8fb79e77f2e8b70cc6d417b58538ef1c498e4497
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 14:54:45 +0100

    added more ids to memorialModel

--- apps/api/src/app/memorials/memorial.model.ts
+++ apps/api/src/app/memorials/memorial.model.ts
@@ -1,5 +1,8 @@
 export class Memorial {
   _id: string | undefined;
+  userId: string | undefined;
+  personId: string | undefined;
+  funeralId: string | undefined;
   description: string | undefined;
   imageBase64: string | undefined;
 }
