f3b975299da21c0e19e1b2816d6ab72a31c19073
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 15:18:46 +0100

    added routes to memorialsController

--- apps/api/src/app/memorials/memorials.controller.ts
+++ apps/api/src/app/memorials/memorials.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { MemorialsService } from './memorials.service';
 
 @Controller('memorials')
 export class MemorialsController {
   constructor(private memorialsService: MemorialsService) {}
+
+  @Post()
+  addMemorial() {}
+
+  @Get()
+  getAllMemorials() {}
+
+  @Get(':id')
+  getMemorialById() {}
+
+  @Get('/funeral/:id')
+  getMemorialByFuneralId() {}
+
+  @Put(':id')
+  updateMemorial() {}
+
+  @Delete(':id')
+  removeMemorialById() {}
 }
