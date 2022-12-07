2e388a058113c1997c128cf822cbdd672e6b13b9
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 14:55:44 +0100

    added class validation to memorialDto

--- apps/api/src/app/memorials/memorial.dto.ts
+++ apps/api/src/app/memorials/memorial.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class MemorialDto {
+  @IsNotEmpty()
+  @IsString()
   personId: string | undefined;
+
+  @IsNotEmpty()
+  @IsString()
   funeralId: string | undefined;
+
+  @IsNotEmpty()
+  @IsString()
   description: string | undefined;
+
+  @IsNotEmpty()
+  @IsString()
   imageBase64: string | undefined;
 }
