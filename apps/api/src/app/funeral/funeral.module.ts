2835cb3785f87ed9da1aa4d6214ca36e545c5a77
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 11:31:55 +0100

    added objects to funeralModule

--- apps/api/src/app/funeral/funeral.module.ts
+++ apps/api/src/app/funeral/funeral.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FuneralSchema } from './funeral.model';
+import { FuneralsController } from './funerals.controller';
+import { FuneralsService } from './funerals.service';
 
 @Module({
-  imports: [],
-  providers: [],
-  controllers: [],
+  imports: [
+    MongooseModule.forFeature([{ name: 'funeral', schema: FuneralSchema }]),
+  ],
+  providers: [FuneralsService],
+  controllers: [FuneralsController],
 })
 export class FuneralModule {}
