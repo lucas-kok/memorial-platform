6a130cffe496c755d26f3aebea5fd6edec22b527
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 17:37:33 +0100

    added jwt guards at messagesController

--- apps/api/src/app/messages/messages.controller.ts
+++ apps/api/src/app/messages/messages.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private messagesService: MessagesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   addMessage() {}
 
@@ -14,9 +16,11 @@ export class MessagesController {
   @Get(':id')
   getMessageById() {}
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   updateMessage() {}
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   removeMessageById() {}
 }
