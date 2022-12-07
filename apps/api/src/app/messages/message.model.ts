008370532149b40d32be5a9ca18c9c15a46dad5b
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 16:46:09 +0100

    added messageModel

---
+++ apps/api/src/app/messages/message.model.ts
@@ -0,0 +1,9 @@
+export class Message {
+  _id: string | undefined;
+  userId: string | undefined;
+  dateTime: Date | undefined;
+  qoute: string | undefined;
+  message: string | undefined;
+  imageBase64: string | undefined;
+  showName: boolean | undefined;
+}
