7574aa8fc5a1d6250cb0a775a3781ac182d0e44a
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 11:29:32 +0100

    added property values to funeralModel

--- apps/api/src/app/funeral/funeral.model.ts
+++ apps/api/src/app/funeral/funeral.model.ts
@@ -1,13 +1,27 @@
-import { Schema } from '@nestjs/mongoose';
+import { Prop, Schema } from '@nestjs/mongoose';
 
 @Schema()
 export class Funeral {
   _id: string | undefined;
+
+  @Prop({ type: 'string' })
   userId: string | undefined;
+
+  @Prop({ type: 'Date' })
   dateTime: Date | undefined;
+
+  @Prop({ type: 'string' })
   adress: string | undefined;
+
+  @Prop({ type: 'string' })
   postalCode: string | undefined;
+
+  @Prop({ type: 'string' })
   city: string | undefined;
+
+  @Prop({ type: 'string' })
   description: string | undefined;
+
+  @Prop({ type: 'boolean' })
   isPrivate: boolean | undefined;
 }
