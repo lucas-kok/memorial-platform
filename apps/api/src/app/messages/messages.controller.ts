ce6057ba3c284154679cfbc70f1d103c95b8d99b
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 17:37:08 +0100

    added routes to messagesController

--- apps/api/src/app/messages/messages.controller.ts
+++ apps/api/src/app/messages/messages.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private messagesService: MessagesService) {}
+
+  @Post()
+  addMessage() {}
+
+  @Get()
+  getMessageFromMemorialId() {}
+
+  @Get(':id')
+  getMessageById() {}
+
+  @Put(':id')
+  updateMessage() {}
+
+  @Delete(':id')
+  removeMessageById() {}
 }
