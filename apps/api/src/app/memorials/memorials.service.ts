b749c6eb0de04a222a29f3a218661e8738d21509
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 15:06:33 +0100

    injected memorialModel into memorialsService

--- apps/api/src/app/memorials/memorials.service.ts
+++ apps/api/src/app/memorials/memorials.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MemorialDocument } from './memorial.model';
 
 @Injectable()
 export class MemorialsService {
+  constructor(
+    @InjectModel('memorial')
+    private readonly memorialModel: Model<MemorialDocument>
+  ) {}
+
   addMemorial() {}
 
   getAllMemorials() {}
