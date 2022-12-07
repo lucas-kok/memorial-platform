89e25003a4d825ed59a07dbc7dcb009fa4b6ca6c
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 11:19:41 +0100

    added funeralDto

---
+++ apps/api/src/app/funeral/funeral.dto.ts
@@ -0,0 +1,8 @@
+export class FuneralDto {
+  readonly dateTime: Date | undefined;
+  readonly adress: string | undefined;
+  readonly postalCode: string | undefined;
+  readonly city: string | undefined;
+  readonly description: string | undefined;
+  readonly isPrivate: boolean | undefined;
+}
