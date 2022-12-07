f736b6fe03a740ae8a94258ef55288e5593cd28e
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 17:02:34 +0100

    added functionality to add messages in messagesService

--- apps/api/src/app/messages/messages.service.ts
+++ apps/api/src/app/messages/messages.service.ts
@@ -1,10 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MemorialDto } from '../memorials/memorial.dto';
+import { MemorialsService } from '../memorials/memorials.service';
+import { MessageDto } from './message.dto';
+import { Message, MessageDocument } from './message.model';
 
 @Injectable()
 export class MessagesService {
-  constructor() {}
+  constructor(
+    @InjectModel('message')
+    private readonly messageModel: Model<MessageDocument>,
+    private memorialsService: MemorialsService
+  ) {}
 
-  addMessageToMemorial() {}
+  async addMessage(
+    messageDto: MessageDto,
+    userId: string,
+    memorialId: string
+  ): Promise<Message> {
+    Logger.log(
+      '[MessagesService] addMessage(' +
+        memorialId +
+        ') called with userId: {' +
+        userId +
+        '}'
+    );
+
+    const message = {
+      userId,
+      memorialId,
+      ...messageDto,
+    };
+
+    return await this.messageModel.create(message);
+  }
 
   updateMessage() {}
 
