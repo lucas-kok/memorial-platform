71e5228cafd2c88f8ee2a4ad4af025aa3e2f64fd
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 12:00:23 +0100

    made route of getFuneralById communicate with funeralsService

--- apps/api/src/app/funeral/funerals.controller.ts
+++ apps/api/src/app/funeral/funerals.controller.ts
@@ -14,6 +14,7 @@ import {
 import { Response } from 'express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { IGetUserAuthInfoReqeust } from '../shared/auth.inforequest.interface';
+import { IdValidator } from '../shared/id.validator';
 import { FuneralDto } from './funeral.dto';
 import { FuneralsService } from './funerals.service';
 
@@ -55,8 +56,28 @@ export class FuneralsController {
   }
 
   @Get(':id')
-  getFuneralById(@Param('id') id: string) {
+  async getFuneralById(@Param('id') id: string, @Res() res: Response) {
     Logger.log('[FuneralsController][GET]/funerals/' + id + ' called');
+
+    if (!IdValidator.validate(id)) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Id is not in a valid string format',
+      });
+    }
+
+    const funeral = await this.funeralsService.getFuneralById(id);
+    if (!funeral) {
+      return res.status(404).json({
+        status: 404,
+        error: 'Funeral with id: {' + id + '} not found',
+      });
+    }
+
+    return res.status(200).json({
+      status: 200,
+      result: funeral,
+    });
   }
 
   @UseGuards(JwtAuthGuard)
