3f1a07618ce342a800a8d555aa2b2be67774af95
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 15:16:14 +0100

    added memorialsService to memorialModule

--- apps/api/src/app/memorials/memorial.module.ts
+++ apps/api/src/app/memorials/memorial.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MemorialSchema } from './memorial.model';
+import { MemorialsService } from './memorials.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'memorial', schema: MemorialSchema }]),
   ],
-  providers: [],
+  providers: [MemorialsService],
   controllers: [],
 })
 export class MemorialModule {}
