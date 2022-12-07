85ad4c178fe565ec2deb1baaf5b4326419f3fbad
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 16:15:39 +0100

    added functionality to the route createMemorial in memorialsController

--- apps/api/src/app/memorials/memorials.controller.ts
+++ apps/api/src/app/memorials/memorials.controller.ts
@@ -1,12 +1,46 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Post,
+  Put,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { IGetUserAuthInfoReqeust } from '../shared/auth.inforequest.interface';
+import { MemorialDto } from './memorial.dto';
 import { MemorialsService } from './memorials.service';
 
 @Controller('memorials')
 export class MemorialsController {
   constructor(private memorialsService: MemorialsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  addMemorial() {}
+  async addMemorial(
+    @Body() memorialDto: MemorialDto,
+    @Req() req: IGetUserAuthInfoReqeust,
+    @Res() res: Response
+  ) {
+    Logger.log('[MemorialsController][POST]/memorials called');
+    Logger.log(memorialDto);
+
+    const requestId = req.user._id;
+    const memorial = await this.memorialsService.addMemorial(
+      memorialDto,
+      requestId
+    );
+
+    return res.status(201).json({
+      status: 201,
+      result: memorial,
+    });
+  }
 
   @Get()
   getAllMemorials() {}
