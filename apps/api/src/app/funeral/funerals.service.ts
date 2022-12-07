d8b15d3f72462ba43ec2ab6fd52374b4c880ca5d
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 11:23:01 +0100

    added basic funeralsSerivce'

---
+++ apps/api/src/app/funeral/funerals.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class FuneralsService {
+  addFuneral() {}
+
+  getAllFunerals() {}
+
+  getFuneralById() {}
+
+  updateFuneral() {}
+
+  removeFuneralById() {}
+}
