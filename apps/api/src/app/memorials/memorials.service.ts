4a37e151a0b00933feec195f955ac6e2972624d2
lucas-kok <lucas.kok@hotmail.nl>
2022-12-07 15:02:47 +0100

    added default methods in memorialsService

--- apps/api/src/app/memorials/memorials.service.ts
+++ apps/api/src/app/memorials/memorials.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MemorialsService {}
+export class MemorialsService {
+  addMemorial() {}
+
+  getAllMemorials() {}
+
+  getMemorialById() {}
+
+  getMemorialFromFuneral() {}
+
+  updateMemorial() {}
+
+  removeMemorialById() {}
+}
