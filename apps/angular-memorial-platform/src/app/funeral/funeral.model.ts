68fbc0395a664871870be03b68628097fd41b4d3
lucas-kok <lucas.kok@hotmail.nl>
2023-04-08 09:51:33 +0200

    added funeralService and model with getAllFunerals method

---
+++ apps/angular-memorial-platform/src/app/funeral/funeral.model.ts
@@ -0,0 +1,10 @@
+export class Funeral {
+  _id: string | undefined;
+  userId: string | undefined;
+  dateTime: Date | undefined;
+  adress: string | undefined;
+  postalCode: string | undefined;
+  city: string | undefined;
+  description: string | undefined;
+  isPrivate: boolean | undefined;
+}
---
+++ apps/angular-memorial-platform/src/app/funeral/funeral.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, isDevMode } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { prodEnvironment } from '../../environments/environment.prod';
+import { Funeral } from './funeral.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FuneralService {
+  apiUri: string | undefined;
+
+  constructor(private http: HttpClient) {
+    this.apiUri = isDevMode() ? environment.API_URL : prodEnvironment.API_URL;
+  }
+
+  getAllFunerals(): Observable<Funeral[]> {
+    return this.http.get<Funeral[]>(this.apiUri! + '/funerals');
+  }
+}
