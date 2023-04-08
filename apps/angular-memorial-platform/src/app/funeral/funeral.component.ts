bb568e11cc78bd85dd5967146e194fb863a09892
lucas-kok <lucas.kok@hotmail.nl>
2023-04-08 09:56:56 +0200

    implement getAllFunerals in funeralComponent

--- apps/angular-memorial-platform/src/app/funeral/funeral.component.ts
+++ apps/angular-memorial-platform/src/app/funeral/funeral.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { map, Subscription, tap } from 'rxjs';
+import { Funeral } from './funeral.model';
+import { FuneralService } from './funeral.service';
 
 @Component({
   selector: 'app-funeral',
   templateUrl: './funeral.component.html',
   styleUrls: ['./funeral.component.css'],
 })
-export class FuneralComponent {}
+export class FuneralComponent {
+  funerals: Funeral[] | undefined;
+  subscription: Subscription | undefined;
+
+  constructor(private funeralService: FuneralService) {}
+
+  ngOnInit() {
+    this.subscription = this.funeralService
+      .getAllFunerals()
+      .pipe(
+        map((res: any) => res),
+        tap((res) => {
+          this.funerals = res.result;
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
+}
